9daa2274ffd8daeae2454d810b994412077ab04d
ranjithkumarkari <rkari1@cbu.edu>
2024-04-26 00:10:36 -0500

    ES4

--- src/ex_4_2.py
+++ src/ex_4_2.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 
 def logstamp_to_datetime(datestr):
-
+    """
+    Convert a logstamp string to a datetime object.
+    """
     if isinstance(datestr, datetime):  # Check if the input is already a datetime object
         return datestr
 
     return datetime.strptime(datestr, '%Y-%m-%dT%H:%M:%S')
-
-
--- src/ex_4_3.py
+++ src/ex_4_3.py
@@ -1,22 +1,15 @@
-""" ex_4_3.py """
-import os
-from datetime import datetime, timedelta
 
-try:
-    from src.ex_4_0 import get_shutdown_events
-    from src.ex_4_2 import logstamp_to_datetime
-    from src.util import get_data_file_path
-except ImportError:
-    from ex_4_0 import get_shutdown_events
-    from ex_4_2 import logstamp_to_datetime
-    from util import get_data_file_path
+import os
+from datetime import timedelta
+from src.ex_4_0 import get_shutdown_events
+from src.ex_4_2 import logstamp_to_datetime
+from src.util import get_data_file_path
 
 # Use this FILENAME variable to test your function.
 FILENAME = get_data_file_path("messages.log")
 # >>>> DO NOT MODIFY CODE ABOVE <<<<
 
 def time_between_shutdowns(logfile):
-
     shutdown_events = get_shutdown_events(logfile)
 
     if not shutdown_events:  # If no shutdown events found
@@ -34,5 +27,3 @@ def time_between_shutdowns(logfile):
 # >>>> The code below will call your function and print the results
 if __name__ == "__main__":
     print(f'{time_between_shutdowns(FILENAME)=}')
-
-
